d2fa92b9667628769d2a84be95f8945edb71ca62
Dustin-RS <omirzak.d@phystech.edu>
2021-05-08 01:36:37 +0300

    Added tests

---
+++ Tests.py
@@ -0,0 +1,20 @@
+import unittest
+import WeatherParser
+
+
+class TestWeatherParserMethods(unittest.TestCase):
+    def test_get_desc_emoji_1(self):
+        self.assertEqual(WeatherParser.get_desc_emoji("overcast clouds"), '☁️')
+
+    def test_get_desc_emoji_2(self):
+        self.assertEqual(WeatherParser.get_desc_emoji("light rain"), '🌦️')
+
+    def test_get_desc_emoji_3(self):
+        self.assertEqual(WeatherParser.get_desc_emoji("heavy snow"), '❄️')
+
+    def test_gettime_from_datetime(self):
+        self.assertEqual(WeatherParser.gettime_from_datetime("2001-08-20 00:12:15"), "00:12:15")
+
+
+if __name__ == '__main__':
+    unittest.main()
