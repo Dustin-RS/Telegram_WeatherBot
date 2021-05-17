155b30566a92c60c539ff5bde4a21c712831383c
Dustin-RS <omirzak.d@phystech.edu>
2021-05-17 22:48:47 +0300

    Removed token from github

--- Constants.py
+++ Constants.py
@@ -1,4 +1,3 @@
-BOT_TOKEN = '1695565688:AAGW95Q4N0MXGC1E-sscQ171dNa_dII1aiY'  # bot token from @BotFather.
 WEATHER_TOKEN = "ff8358314800130efdd64202906f389f"  # bot token from pyowm.
 CITY = "Moscow"  # Constant for home city.
 CHG_CITY = False  # If 'change home city' option was chosen.
