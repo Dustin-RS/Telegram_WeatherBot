b3bd60d3b8243d7aca60bbe16c8dae58bd2c9d55
Dastan <79257827+Dustin-RS@users.noreply.github.com>
2022-02-17 19:43:01 +0300

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,4 +1,4 @@
-WEATHER_TOKEN = "ff8358314800130efdd64202906f389f"  # bot token from pyowm.
+WEATHER_TOKEN = ""  # bot token from pyowm.
 CITY = "Moscow"  # Constant for home city.
 CHG_CITY = False  # If 'change home city' option was chosen.
 FROM_KELVIN_TO_CELSIUS = 273.15  # A constant which is need to subtract from value.
